721863536a65f9723eddfd1eff0d8554a80de9de
Délita Makanda <delita.makanda@gmail.com>
2017-09-26 19:35:36 +0200

    add order to module

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -31,9 +32,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules')
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
+        return '{}. {}'.format(self.order, self.title)
 
 
 class ItemBase(models.Model):
@@ -66,3 +71,7 @@ class Content(models.Model):
     content_type = models.ForeignKey(ContentType, limit_choices_to={'model__in':('text', 'video', 'image', 'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
