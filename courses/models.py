71f756d5ecd5e0e2eb0678a0c219fe49a9bc8ecf
Délita Makanda <delita.makanda@gmail.com>
2017-09-26 18:49:44 +0200

    add models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, related_name='courses_created')
+    subject = models.ForeignKey(Subject, related_name='courses')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, related_name='modules')
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
