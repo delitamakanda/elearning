b71bf9f75645fef63eda8528e188dc787794654c
Délita Makanda <delita.makanda@gmail.com>
2017-10-08 02:06:10 +0200

    add default route for course

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,12 +1,31 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
 from django.shortcuts import get_object_or_404
-from ..models import Subject
-from .serializers import SubjectSerializer
+from ..models import Subject, Course
+from .serializers import SubjectSerializer, CourseSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
