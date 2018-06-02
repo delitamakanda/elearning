dd772aadeea45e0924f40ad780af033ddcfb7b6c
Délita Makanda <delita.makanda@gmail.com>
2018-06-02 23:29:22 +0200

    add super fast search form

--- students/views/classroom.py
+++ students/views/classroom.py
@@ -1,5 +1,30 @@
+import re
+
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from courses.models import Course
+
+def normalize_query(query_string, findterms=re.compile(r'"([^"]+)"|(\S+)').findall, normspace=re.compile('\s{2,}').sub):
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
+def get_query(query_string, search_fields):
+    query = None
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+    return query
+
 
 class SignupView(TemplateView):
     template_name = 'registration/signup.html'
@@ -10,4 +35,16 @@ def index(request):
             return redirect('teacher_quiz_change_list')
         else:
             return redirect('student_quiz_list')
-    return render(request, 'students/index.html')
+
+    query_string = ''
+    found_results = None
+
+    if ('q' in request.GET) and request.GET['q'].strip():
+        query_string = request.GET['q']
+        result_query = get_query(query_string, ['title', 'overview', ])
+        found_results = Course.objects.filter(result_query).order_by('-created')
+
+    return render(request, 'students/index.html', {
+        'query_string': query_string,
+        'found_results': found_results
+    })
