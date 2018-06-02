8f5d6a6a66102fadf4d01d653ef7cdf472a4e989
Délita Makanda <delita.makanda@gmail.com>
2018-06-03 00:14:07 +0200

    gulpify project for frontend

---
+++ frontend/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var iconfont = require('gulp-iconfont');
+var consolidate = require('gulp-consolidate');
+
+gulp.task('iconfont', function () {
+    gulp.src('icons/**/*.svg')
+    .pipe(iconfont({
+        fontName: 'custom',
+        centerHorizontally: true,
+        normalize: true,
+        prependUnicode: true
+    })).on('glyphs', function (glyphs) {
+    gulp.src('scss/templates/_icons.scss')
+        .pipe(consolidate('lodash', {
+            glyphs: glyphs,
+            fontName: 'custom',
+            fontPath: '../fonts/custom/',
+            className: 'icon'
+        }))
+        .pipe(gulp.dest('scss'));
+    })
+    .pipe(gulp.dest('../courses/static/fonts/custom'));
+});
+
+gulp.task('sass', function () {
+    gulp.src('scss/**/*.scss')
+    .pipe(sass({
+      indentWidth: 4,
+      outputStyle: 'expanded'
+    }))
+    .pipe(gulp.dest('../courses/static/styles'));
+});
+
+gulp.task('default', ['sass'], function () {
+    gulp.watch('scss/**/*.scss', ['sass']);
+});
