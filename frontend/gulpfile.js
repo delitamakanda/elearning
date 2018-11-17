b02a716b606c3ed2a28d66f66cffdf8d03f9cb4c
Délita Makanda <delita.makanda@gmail.com>
2018-11-17 23:16:50 +0100

    [style] sassify refactor

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,8 +1,47 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var iconfont = require('gulp-iconfont');
-var consolidate = require('gulp-consolidate');
-var imagemin = require('gulp-imagemin');
+var gulp        = require('gulp'),
+    gulpif      = require('gulp-if'),
+    browserSync = require('browser-sync'),
+    sass        = require('gulp-sass'),
+    prefix      = require('gulp-autoprefixer'),
+    imagemin    = require('gulp-imagemin'),
+    reload      = browserSync.reload,
+    plumber     = require('gulp-plumber'),
+    uglify      = require('gulp-uglify'),
+    concatJs    = require('gulp-concat'),
+    folders     = require('gulp-folders'),
+    path        = require('path'),
+    sourcemaps = require('gulp-sourcemaps'),
+    iconfont = require('gulp-iconfont'),
+    consolidate = require('gulp-consolidate');
+
+function onError(err) {
+    console.log(err);
+}
+
+var paths = {
+    sassSrc:        './scss/',
+    cssDir:         '../courses/static/styles/',
+    imgSrc:         './images/',
+    imgDir:         '../courses/static/images/',
+    jsCoreSrc:      './scripts/',
+    jsDir:          '../courses/static/scripts/',
+    jsLibSrc:       './scripts/libs/',
+    jsLibDir:       '../courses/static/scripts/libs/',
+    tplSrc:         '../courses/templates/'
+}
+
+
+gulp.task('serve', ['sass'], function() {
+    browserSync({
+        open: false,
+        proxy: "http://localhost:8000/"
+    });
+    gulp.watch(paths.sassSrc+'*.scss', ['sass']);
+    gulp.watch(paths.sassSrc+'/**/*.scss', ['sass']);
+    gulp.watch(paths.jsCoreSrc+'/**/*.js', ['compressCoreJs']);
+    gulp.watch(paths.tplSrc+'/**/*.html').on('change', reload);
+});
+
 
 gulp.task('iconfont', function () {
     gulp.src('icons/**/*.svg')
@@ -25,35 +64,55 @@ gulp.task('iconfont', function () {
     .pipe(gulp.dest('../courses/static/fonts/custom'));
 });
 
+// Compile Sass
 gulp.task('sass', function () {
-    gulp.src('scss/**/*.scss')
+    return gulp.src([paths.sassSrc+'styles.scss'])
+    .pipe(sourcemaps.init())
     .pipe(sass({
-      indentWidth: 4,
-      outputStyle: 'compact'
+        outputStyle: 'compressed',
+        //outputStyle: 'nested',
+        sourceComments: 'normal',
+        errLogToConsole: true,
+        includePaths : [paths.sassSrc]
     }))
-    .pipe(gulp.dest('../courses/static/styles'));
+    .pipe(prefix(
+        'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'
+    ))
+    //.pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.cssDir))
+    .pipe(plumber({
+        errorHandler: onError
+    }))
+    .pipe(reload({stream: true}))
 });
 
-gulp.task('image', function() {
-    gulp.src('images/*')
-    .pipe(imagemin([
-        imagemin.gifsicle({interlaced: true}),
-        imagemin.jpegtran({progressive: true}),
-        imagemin.optipng({optimizationLevel: 5}),
-        imagemin.svgo({
-        plugins: [
-            { removeViewBox: true },
-            { cleanupIDs: false }
-        ]
-    })
-    ]))
-    .pipe(gulp.dest('../courses/static/images'));
+// Compile JS
+gulp.task('compressCoreJs', function() {
+   return gulp.src(paths.jsCoreSrc+'*.js')
+    .pipe(concatJs('main.min.js'))
+    //.pipe(uglify())
+    .pipe(gulp.dest(paths.jsDir))
+    .pipe(reload({stream: true}))
 });
 
-gulp.task('watch', function() {
-    gulp.watch('scss/**/*.scss', ['sass'])
-})
-
-gulp.task('default', ['sass'], function () {
-    gulp.watch('scss/**/*.scss', ['sass', 'watch']);
+// Compile Lib JS
+gulp.task('compressLibJs', function() {
+   return gulp.src(paths.jsLibSrc+'/**/*.min.js')
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.jsLibDir))
+    .pipe(reload({stream: true}))
 });
+
+// Generate IMG
+gulp.task('images', function() {
+   return gulp.src(paths.imgSrc)
+    .pipe(imagemin())
+    .pipe(gulpif('*.png', gulp.dest(paths.imgDir)))
+    .pipe(gulpif('*.svg', gulp.dest(paths.imgDir)))
+    .pipe(gulpif('*.jpg', gulp.dest(paths.imgDir)))
+    .pipe(gulpif('*.gif', gulp.dest(paths.imgDir)))
+    .pipe(reload({stream: true}))
+});
+
+gulp.task('sprites', ['images']);
+gulp.task('default', ['serve', 'sprites', 'sass', 'compressCoreJs', 'compressLibJs']);
---
+++ frontend/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$(function() {
+
+    // navbar dropdown
+    var $navbarBurgers = $('body').find('.navbar-burger');
+
+    if ($navbarBurgers.length > 0) {
+        $navbarBurgers.each(function(i){
+            $(this).on('click', function(e) {
+                e.preventDefault();
+
+                var target = $(this).data('target'),
+                    $target = $('#sidebar');
+
+                $(this).toggleClass('is-active');
+                $target.toggleClass('is-active');
+            });
+        });
+    }
+});
