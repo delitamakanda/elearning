0b1388aed2186675c7ba242cbaaf6c31fcc82ccd
Délita Makanda <delita.makanda@gmail.com>
2018-11-24 22:07:49 +0100

    add bootstap

--- frontend/scripts/header.js
+++ frontend/scripts/header.js
@@ -18,17 +18,3 @@ function throttle(fn, delay) {
         }
     };
 }
-
-
-function onScroll () {
-    if (window.pageYOffset) {
-        $header.classList.add('is-active');
-    } else {
-        $header.classList.remove('is-active');
-    }
-}
-
-
-var $header = document.querySelector('.js-header');
-
-window.addEventListener('scroll', throttle(onScroll, 25));
