d99500955216512e1496a5bb065a531ea197eca7
Délita Makanda <delita.makanda@gmail.com>
2019-06-10 20:29:45 +0200

    add courses

--- courses/static/scripts/main.js
+++ courses/static/scripts/main.js
@@ -2431,6 +2431,78 @@ function throttle(fn, delay) {
     };
 }
 
+var elearningApp = angular.module('elearningApp', []);
+
+elearningApp.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+    // Messages array
+    $scope.messages = {};
+
+    // retrieve Messages from the restAPI
+    $http({
+        method: 'GET',
+        url: '/stored-messages/inbox/'
+    })
+    .success(function (data, status, headers, config) {
+        $scope.messages = data;
+    })
+    .error(function (data, status, headers, config) {
+        // something went wrong :(
+    });
+
+    // mark messages read
+    $scope.markRead = function (index) {
+        var id = $scope.messages[index].id;
+        $http({
+            method: 'POST',
+            url: '/stored-messages/inbox/'+id+'/read/',
+            xsrfHeaderName: 'X-CSRFToken',
+            xsrfCookieName: 'csrftoken'
+        })
+        .success(function (data, status, headers, config) {
+            $scope.messages.splice(index, 1);
+        })
+        .error(function (data, status, headers, config) {
+            // something went wrong :(
+        })
+    };
+
+    // mark all read
+    $scope.markAllRead = function () {
+        $http({
+            method: 'POST',
+            url: '/stored-messages/mark_all_read/',
+            xsrfHeaderName: 'X-CSRFToken',
+            xsrfCookieName: 'csrftoken'
+        })
+        .success(function(data, status, headers, config) {
+            $scope.messages.splice(0, $scope.messages.length);
+        })
+        .error(function(data, status, headers, config){
+            // something went wrong :(
+        })
+    };
+    // send a Broadcast Message
+    $scope.sendBroadcast = function () {
+        $http({
+            method: 'GET',
+            url: '/stored-messages/broadcast/',
+        })
+        .success(function(data, status, headers, config) {
+	    // retrieve Messages from the restAPI
+	    $http({
+		method: 'GET',
+		url: '/stored-messages/inbox/'
+	    })
+	    .success(function (data, status, headers, config) {
+		$scope.messages = data;
+	    })
+        })
+        .error(function(data, status, headers, config){
+            // something went wrong :(
+        })
+    };
+}]);
+
 var isOffline = false;
 window.addEventListener('load', checkConnectivity);
 
