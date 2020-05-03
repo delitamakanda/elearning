123db8d94d9cc1f0cc5289426db566a732c20b28
Délita Makanda <delita.makanda@gmail.com>
2020-05-03 23:31:20 +0200

    script: navbar, flash message

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -12,9 +12,43 @@ $(function() {
                 var target = $(this).data('target'),
                     $target = $('#sidebar');
 
-                $(this).toggleClass('is-active');
                 $target.toggleClass('is-active');
             });
         });
     }
+
+    // flash message
+    var $flashMessage = $('.alert');
+	if ($flashMessage.length === 0) {
+		return;
+	}
+
+	var FLASH_MESSAGE_DELAY_BEFORE_HIDE = 8000;
+
+	// Declare
+	var hideOne = function ($flashMessage) {
+		$flashMessage.slideUp(function () {
+			$(this).detach();
+		});
+	};
+
+	var launchTimer = function () {
+		window.timerFlashMessage = setTimeout(function () {
+			$flashMessage.each(function () {
+				hideOne($(this));
+			});
+		}, FLASH_MESSAGE_DELAY_BEFORE_HIDE);
+	};
+
+	// Init
+	var $closeButton = $flashMessage.find('.close');
+
+	if ($flashMessage.data('is-auto-close')) {
+		launchTimer();
+	}
+
+	// Events
+	$closeButton.on('click', function () {
+		hideOne($(this).parents('.alert'));
+	});
 });
