c7c3316cef0510bbeb91abd70ad18078604354b8
Délita Makanda <delitamakanda@users.noreply.github.com>
2025-01-06 16:54:15 +0100

    Update forms.py

--- students/forms.py
+++ students/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.utils.text import capfirst
-from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.translation import gettext_lazy, gettext as _
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
 from django.forms.utils import ValidationError
