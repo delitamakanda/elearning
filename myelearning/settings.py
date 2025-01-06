ef5a7adf7dba97d99a2f8243b77f02e1d11a75b1
Délita Makanda <delitamakanda@users.noreply.github.com>
2025-01-06 17:14:14 +0100

    Update settings.py

--- myelearning/settings.py
+++ myelearning/settings.py
@@ -172,7 +172,7 @@ AUTHENTICATION_BACKENDS = [
 # Cache
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
         'LOCATION': '127.0.0.1:11211',
     }
 }
